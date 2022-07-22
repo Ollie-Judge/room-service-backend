9b0bdb61d5e674dbf725f8b2510c8d99e33ed2e0
Ollie-Judge <ollie.judge@hotmail.com>
2022-07-22 10:01:57 +0100

    added an additional route of food

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
+import foodRoutes from "./routes/routes.js";
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  console.log("[test the home route]");
-  res.send("Welcome to the homepage");
-});
+app.use("/food", foodRoutes);
+
+app.get("/", (req, res) => res.send("Welcome to the homepage"));
 
 app.listen(PORT, () =>
   console.log(`Server is running on http://localhost:${PORT}`)
