bd2bb9f6667b994e0e9bf7671d88e0129038dd0c
Ollie-Judge <ollie.judge@hotmail.com>
2022-07-22 10:27:39 +0100

    made post requests to local host using postman

--- routes/routes.js
+++ routes/routes.js
@@ -8,11 +8,26 @@ const food = [
     description: "pasta, rich tomoato sauce with vegetables and ground meat",
     price: 18,
   },
+  {
+    name: "Lasagna",
+    description: "like sphagetti bolognaise just not as good",
+    price: 15,
+  },
 ];
 
 router.get("/", (req, res) => {
   console.log(food);
   res.send(food);
+});
+
+router.post("/", (req, res) => {
+  console.log("POST ROUTE REACHED");
+
+  const foodItem = req.body;
+
+  food.push(foodItem);
+
+  res.send(`A food of ${foodItem.name} has been added to the database`);
 });
 
 export default router;
