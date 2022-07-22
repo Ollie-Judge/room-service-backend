29c813dfa4c2c37c4f7eecd80da3962ced514993
Ollie-Judge <ollie.judge@hotmail.com>
2022-07-22 10:08:03 +0100

    got console.log of manual json data from food page

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,16 @@ import express from "express";
 
 const router = express.Router();
 
+const food = [
+  {
+    name: "Sphagetti bolognaise",
+    description: "pasta, rich tomoato sauce with vegetables and ground meat",
+    price: 18,
+  },
+];
+
 router.get("/", (req, res) => {
+  console.log(food);
   res.send("Hello from the food page");
 });
 
