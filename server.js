5a1fd89bdf7bfde9e841425c0532440efe9882f9
Ollie-Judge <ollie.judge@hotmail.com>
2022-07-22 09:49:04 +0100

    added a home route in server.js

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  console.log("[test the home route]");
+  res.send("Welcome to the homepage");
+});
+
 app.listen(PORT, () =>
   console.log(`Server is running on http://localhost:${PORT}`)
 );
